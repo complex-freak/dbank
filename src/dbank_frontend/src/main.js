55dc23f5c8724041fbfecfb21ab2c7437c71c543
complex-freak <piuseverin06@hotmail.com>
2024-10-15 19:56:20 +0300

    "Updated DBank backend and frontend code: added functionality to DBank actor, modified HTML and CSS files, and updated JavaScript files to interact with the DBank actor."

--- src/dbank_frontend/src/main.js
+++ src/dbank_frontend/src/main.js
@@ -1,4 +1,11 @@
-import App from './App';
-import './index.scss';
+// import App from './App';
+// import './index.scss';
 
-const app = new App();
+// const app = new App();
+
+import { dbank_backend } from '../../declarations/dbank_backend';
+
+window.addEventListener("load", async () => {
+  const currentValue = await dbank_backend.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentValue * 100) /100;
+})
